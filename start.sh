f772dc35928a607f69b398b0e234cf87bc4bdb7f
adisakwamalun22-prog <adisak.wamalun22@gmail.com>
2025-11-09 13:10:31 +0700

    FIXED: Added missing packages to requirements.txt

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# 1. ตั้งค่า FLASK_APP
 export FLASK_APP=app.py
-
-# 2. รัน Gunicorn โดยใช้ Python Interpreter ที่ Render สร้างไว้โดยตรง
-# $VIRTUAL_ENV คือตัวแปรที่ชี้ไปที่โฟลเดอร์ venv
-exec $VIRTUAL_ENV/bin/gunicorn app:app
+exec gunicorn app:app -b 0.0.0.0:$PORT
